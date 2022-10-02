7cdefc075fd181896848a0d00d4c53ac6152f583
Varsha reddy Yasa <varsha@Varshas-MacBook-Pro.local>
2022-10-01 19:07:26 -0500

    feat(0): adding typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mssql',
+    host: 'localhost',
+    port: 1433,
+    username: 'sa',
+    password: '<YourStrong@Passw0rd>',
+    database: 'Loyalty',
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    synchronize: false,
+    options: { encrypt: false },
+    cli: {
+      migrationsDir: "src/db/migration"
+    }
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
